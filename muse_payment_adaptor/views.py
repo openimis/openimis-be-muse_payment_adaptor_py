694c867134fddc244b61bfb1be663f890c4d219f
Hiren Soni <hirensoni913@gmail.com>
2022-10-19 18:14:36 +0300

    Created 2 empty  endpoints for acknowledgement and response

--- muse_payment_adaptor/views.py
+++ muse_payment_adaptor/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+def bulk_payment_sub_req_ack(request):
+    return Response({"data": request.data})
+
+
+@api_view(['POST'])
+def bulk_payment_sub_resp(request):
+    return Response({"data": request.data})
