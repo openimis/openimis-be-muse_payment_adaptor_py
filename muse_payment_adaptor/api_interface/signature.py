56636eebfdd0bb933e35a5d99d5c50c1ecbc4120
Kamil Malinowski <kmalinowski@soldevelo.com>
2022-11-02 10:46:35 +0100

    OTC-727 Implemented signature handling

---
+++ muse_payment_adaptor/api_interface/signature.py
@@ -0,0 +1,29 @@
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+from muse_payment_adaptor.apps import MusePaymentAdaptorConfig
+
+__padding = padding.PSS(mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH)
+__algorithm = hashes.SHA1()
+
+
+def create_signature(message: bytes) -> bytes:
+    certificate = MusePaymentAdaptorConfig.certificate
+    signature = certificate.key.sign(message, padding=__padding, algorithm=__algorithm)
+    return signature
+
+
+def verify_signature(message: bytes, signature: bytes) -> bool:
+    certificate = MusePaymentAdaptorConfig.certificate
+    public_key = certificate.key.public_key()
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding=__padding,
+            algorithm=__algorithm
+        )
+        return True
+    except InvalidSignature:
+        return False
---
+++ muse_payment_adaptor/tests/signature.py
@@ -0,0 +1,32 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+from base64 import b64encode
+
+from muse_payment_adaptor.api_interface.signature import create_signature, verify_signature
+from muse_payment_adaptor.tests.helpers.signature_data import test_message, test_certificate, test_signature, \
+    test_wrong_signature
+from muse_payment_adaptor.apps import MusePaymentAdaptorConfig
+
+
+class SignatureTests(TestCase):
+    pass
+
+    @classmethod
+    def setUpClass(cls):
+        super(SignatureTests, cls).setUpClass()
+
+    @patch.object(MusePaymentAdaptorConfig, 'certificate', new=test_certificate)
+    def test_sign_message(self):
+        signature = create_signature(test_message)
+        self.assertTrue(signature)
+        self.assertTrue(verify_signature(test_message, signature))
+
+    @patch.object(MusePaymentAdaptorConfig, 'certificate', new=test_certificate)
+    def test_verify_signature(self):
+        self.assertTrue(verify_signature(test_message, test_signature))
+
+    @patch.object(MusePaymentAdaptorConfig, 'certificate', new=test_certificate)
+    def test_verify_signature_fail(self):
+        self.assertFalse(verify_signature(test_message, test_wrong_signature))
+
